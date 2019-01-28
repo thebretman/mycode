2e0e83f090682f132ddfb3e8ba679ec3c207c7a4
Bret Smith <elguitarbeast@gmail.com>
2019-01-28 15:32:50 -0500

    your commit message

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,5 @@
+#!/usr/gin/env python3
+my_list = [ "192.16666668.0.5", 5060, "UP" ]
+print("The first itemin the list (IP): " +my_list[0] )
+print("The last item in the list (state):" +my_list[2] )
+
